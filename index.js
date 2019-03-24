7346214031278e607f4af2c6be31c4cc4ef3f2cf
anas.qudaih <43370089+qudaih69@users.noreply.github.com>
2019-03-24 12:55:39 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+﻿
+﻿
+function sleep(ms) {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+​
+var limitPage = Number($('.active li:nth-child(10)').find('span').text().trim());
+var limitLi = $('.search-results__list').children().length;
+​
+for (let next = 0 ; next <= limitPage; next++) {
+    await sleep(5000);
+​
+    for (let add = 1; add <= limitLi; add++) {
+        $('html, body').animate({ scrollTop:  $('.search-results__list li:nth-child(' + add + ')').offset().top}, 'slow');
+        $('.search-results__list li:nth-child(' + add + ')').css({
+            backgroundColor: '#FAEBD7'
+        })
+        await sleep(500);
+        if($('.search-results__list li:nth-child(' + add + ')').find('button').text().trim() === 'Se connecter') {
+            $('.search-results__list li:nth-child(' + add + ')').css({
+                backgroundColor: '#87CEEB'
+            })
+            $('.search-results__list li:nth-child(' + add + ')').find('button').click();
+            await sleep(1000);
+            $('.button-primary-large').click();
+            await sleep(1000);
+        }
+    }
+​
+    await sleep(500);
+    if(next < limitPage){
+        $('.artdeco-button__text').click();
+    } 
+}
